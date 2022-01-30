251c28d607008a33dfba434753a500013b8be1ac
Ali <kurbanovmuratali@gmail.com>
2022-01-30 13:35:21 +0600

    Updates

---
+++ precache-manifest.6d07513a131db3215d52a780eed818d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "320cf86094cc170f650b5ba72124fb66",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "0e10f45f4c7d3ebf58cb",
+    "url": "/react-gh-pages/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "1215d075071ff610d68b",
+    "url": "/react-gh-pages/static/css/main.1a7afaaf.chunk.css"
+  },
+  {
+    "revision": "0e10f45f4c7d3ebf58cb",
+    "url": "/react-gh-pages/static/js/2.40a1bcf7.chunk.js"
+  },
+  {
+    "revision": "94bb18fc003f369f48aeaef752f07200",
+    "url": "/react-gh-pages/static/js/2.40a1bcf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1215d075071ff610d68b",
+    "url": "/react-gh-pages/static/js/main.6c70a73b.chunk.js"
+  },
+  {
+    "revision": "6c1ce1a1e1ae2d6bd292",
+    "url": "/react-gh-pages/static/js/runtime-main.7822fa4f.js"
+  }
+]);
